ff6c2be2cee29cb832d6adc083993e3590a6a138
Tokii05 <162777906+Tokii05@users.noreply.github.com>
2024-05-08 00:39:25 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,26 @@
+let loginForm = document.getElementById("Registration Form");
+// SELECTING STUDENT
+console.log("Selecting Student");
+let selectedStudent = ["New Student", "Old Student"];
+console.log(selectedStudent[1]);
+// STUDENT'S NAME
+console.log("Student's Full Name");
+let fullName = {
+ firstName: "Kenneth",
+ middleName: "Pontillo",
+ lastName: "Legaspi"
+};
+console.log(fullName);
+// AGE
+console.log("Student's Age");
+let Age = "22";
+console.log(Age); 
+// INFORMATION
+console.log("Student's Information");
+let information = {
+    email: "pontilloken@gmail.com"
+   };
+console.log(information);
+// USERNAME & PASSWORD
+let myUsername = document.getElementById('userName');
+console.log(myUsername);
